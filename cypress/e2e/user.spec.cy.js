3c2ac84c47af50b199787f7b567d32d1dee46f2e
Pablo Nunez <nunezbenitezpablo@gmail.com>
2024-05-27 10:41:02 -0400

    create a user with respect to referee type

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -12,22 +12,33 @@ describe('Tests Home/User Refme', () => {
     cy.contains('strong', 'Usuarios').click();
     cy.url().should('include', '/users');
   });
-  it('Test to Create a User Refme', () => {
+  it('Create a User', () => {
     cy.get('button.q-btn-item').find('i.q-icon.material-icons').contains('add').click();
     cy.get('input[aria-label="Nombre"]').type('Gerardo');
     cy.get('input[aria-label="Apellidos"]').type('Entrenador');
     cy.get('input[aria-label="Mail"]').type('gerardo+27@dlab.software');
     cy.get('input[aria-label="Rol"]').click();
     cy.contains('.q-menu .q-item', 'Entrenador').click();
-    // cy.contains('.q-menu .q-item', 'Árbitro').click();
-    // cy.get('input[aria-label="Tipo árbitro"]').click();
-    // cy.contains('Árbitro principal').click();
-    // cy.get('input[aria-label="Categoria"]').click();
-    // cy.contains('fifa ').click();
     cy.contains('button', 'Crear usuario').click();
     cy.visit('https://refme.dlab.software/#/users/');
     cy.wait(5000);
   })
+  it('create a user with respect to referee type', () => {
+    cy.get('button.q-btn-item').find('i.q-icon.material-icons').contains('add').click();
+    cy.get('input[aria-label="Nombre"]').type('Gerardo');
+    cy.get('input[aria-label="Apellidos"]').type('Arbitro');
+    cy.get('input[aria-label="Mail"]').type('gerardo+29@dlab.software');
+    cy.get('input[aria-label="Rol"]').click();
+    cy.contains('.q-menu .q-item', 'Árbitro').click();
+    cy.get('input[aria-label="Tipo árbitro"]').click();
+    cy.contains('Árbitro principal').click();
+    cy.get('input[aria-label="Categoria"]').click();
+    cy.contains('fifa').click();
+    cy.contains('button', 'Crear usuario').click();
+    cy.visit('https://refme.dlab.software/#/users/');
+  })
+
+
   it('Delete user', () => {
     cy.get('button.q-btn.q-btn-item.non-selectable.no-outline.q-btn--flat.q-btn--rectangle.text-secondary.q-btn--actionable.q-focusable.q-hoverable').
     first().click()
