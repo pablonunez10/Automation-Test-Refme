609aed51743243c2d1a416357c83515c23823ec1
pablonuñez10 <nunezbenitezpablo@gmail.com>
2024-05-29 15:19:19 -0400

    Test de subida de videos

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    viewportWidth: 1200,
+    viewportHeight: 800,
     experimentalStudio: true,
   },
 });
